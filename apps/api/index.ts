44f6ce9bab7ea6d5adc3c7bdb614b1c3618ef4ec
bhagathpunna-Biz <bhagath.punna@bizacuity.com>
2026-05-27 16:18:21 +0530

    added tests

--- apps/api/index.ts
+++ apps/api/index.ts
@@ -1,12 +1,32 @@
-import express from "express"
+import "dotenv/config";
+import express from "express";
+
 const app = express();
-import {prismaClient } from "store";
 
-app.post('/wesite',(req,res)=>{
+import { prismaClient } from "store/client";
 
-})
+app.use(express.json());
 
-app.get("/status/:websiteId",(req,res)=>{
-    
-})
+app.post('/website', async (req, res) => {
+    if (!req.body.url) {
+        res.status(411).json({});
+        return;
+    }
+
+    const website = await prismaClient.website.create({
+        data: {
+            url: req.body.url,
+            timeAdded: new Date()
+        }
+    });
+
+    res.json({
+        id:website.id
+    })
+});
+
+app.get("/status/:websiteId", (req, res) => {
+
+});
+
 app.listen(process.env.PORT || 3000);
--- packages/store/index.ts
+++ packages/store/index.ts
@@ -1,4 +1,23 @@
+import "dotenv/config";
+
 import { PrismaClient } from "./generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import pg from "pg";
 
+console.log(process.env.DATABASE_URL);
 
-export const prismaClient = new PrismaClient();
+const connectionString = process.env.DATABASE_URL;
+
+if (!connectionString) {
+    throw new Error("DATABASE_URL is missing");
+}
+
+const pool = new pg.Pool({
+    connectionString,
+});
+
+const adapter = new PrismaPg(pool);
+
+export const prismaClient = new PrismaClient({
+    adapter,
+});
